e1e65833c2a5801424abe2029f196915d736dae5
Renzo Villena <rvillena_21@outlook.com>
2025-11-06 20:12:13 -0500

    Actualización responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,28 @@
-function Navbar() {
+import { useState } from "react";
+import '../App.css';
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className="navbar">
       <div className="logo">TuNombre.dev</div>
-      <nav>
-        <ul className="nav-links">
-          <li><a href="#hero">Inicio</a></li>
-          <li><a href="#about">Sobre mí</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Proyectos</a></li>
-          <li><a href="#contact">Contacto</a></li>
-        </ul>
+
+      {/* Botón Hamburguesa */}
+      <div className={`hamburger ${open ? "active" : ""}`} onClick={() => setOpen(!open)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menú */}
+      <nav className={`nav-links ${open ? "open" : ""}`}>
+        <a href="#hero">Inicio</a>
+        <a href="#about">Sobre mí</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Proyectos</a>
+        <a href="#contact">Contacto</a>
       </nav>
     </header>
   );
 }
-
-export default Navbar;
